144d04e64986f676b36ca8275f9cbd6fc076c52c
jonnytran <jonnytran@github.com>
2017-09-29 01:38:27 -0500

    initial data exploration

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -5,9 +5,6 @@ import logging
 from dotenv import find_dotenv, load_dotenv
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
 def main(input_filepath, output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
